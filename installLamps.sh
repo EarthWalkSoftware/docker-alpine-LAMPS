c275b0fa21951c5fccf3769f810d75e8676cd14e
jay <earthwalksoftware@gmail.com>
2017-09-13 20:13:34 -0600

    modified to use a configuration file for file locations

--- installLamps.sh
+++ installLamps.sh
@@ -1,6 +1,46 @@
 #!/bin/sh
 
+# =========================================================================
+# =========================================================================
+#
+#	installLamps
+#	  Shell script to install the LAMPS scripts and exit.
+#
+# =========================================================================
+#
+# @author Jay Wheeler.
+# @version 0.1.0
+# @copyright © 2017. EarthWalk Software.
+# @license Licensed under the Academic Free License version 3.0
+# @package LAMPS
+# @subpackage installLamps
+#
+# =========================================================================
+#
+#	Copyright © 2017. EarthWalk Software
+#	Licensed under the Academic Free License, version 3.0.
+#
+#	Refer to the file named License.txt provided with the source,
+#	or from
+#
+#		http://opensource.org/licenses/academic.php
+#
+# =========================================================================
+# =========================================================================
+echo "LAMPS=${LAMPS}" >> /lamps-config/lamps-config
+echo "LAMPS_DATA=${LDATA}" >> /lamps-config/lamps-config
+echo "LAMPS_CONFIG=${LCONFIG}" >> /lamps-config/lamps-config
+
+cp -rf /lamps-config/lamps-config $LCONFIG/.lamps
 cp -rf /lamps/. $LAMPS
 
-chmod -R 775 $LAMPS 
+#echo "LAMPS=${LAMPS}" >> $LCONFIG/.lamps
+#echo "LAMPS_DATA=${LDATA}" >> $LCONFIG/.lamps
+#echo "LAMPS_CONFIG=${LCONFIG}" >> $LCONFIG/.lamps
+
+chmod -R 775 $LAMPS
+chmod -R 774 $LCONFIG
+
 chown -R $LUID:$LGID $LAMPS
+chown -R $LUID:$LGID $LCONFIG
+
